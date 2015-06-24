19d5d4d231e8621fd9c3d45736e991d10a51132d
hghmn <nathanwhughes@gmail.com>
2015-06-23 22:12:26 -0700

    Implemented intended features, cleaned up code
    The ability to choose a central latitude and longitude, as well as choose the threshold from the center to look for fires, has been implemented.  From the command line, passing in a lat, lon, and threshold will give you the titles, descriptions, link, and distance to all fires within that range.

--- main.js
+++ main.js
@@ -1,38 +1,95 @@
-// load dependencies
+// ==================================================
+// Is My House On Fire
+// ==================================================
+
+// get feed from inciweb, and do something with it
 var http = require('http'),
   xml2js = require('xml2js');
-  
-//The url we want is: 'http://inciweb.nwcg.gov/feeds/rss/incidents/'
+
+// The url we want is: 'http://inciweb.nwcg.gov/feeds/rss/incidents/'
 var options = {
-  host: 'inciweb.nwcg.gov',
-  path: '/feeds/rss/incidents/'
+  host : 'inciweb.nwcg.gov',
+  path : '/feeds/rss/incidents/',
+  threshold : 100, // 100 km
+  lat : 37.735969, // yosemite valley
+  lon : -119.601631
 };
 
-// set my rough geographical location
-var myGeo = {
-  lat: 38.000,
-  long: -120.000
-};
+// scrape command line arguments
+for ( var i = 0; i < process.argv.length; i+=2 ) {
+  if( /(-t|--threshold)/.test( process.argv[ i ] ) && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
+    // get threshold
+    options.threshold = parseFloat( process.argv[ i + 1 ] );
+  } else if( /(-lat|--latitute)/.test( process.argv[ i ] ) && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
+    // get latitute
+    options.lat = parseFloat( process.argv[ i + 1 ] );
+  } else if( /(-lon|--longitude)/.test( process.argv[ i ] ) && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
+    // get longitude
+    options.lon = parseFloat( process.argv[ i + 1 ] );
+  }
+}
 
-// set the sensitivity to look for
-var sensitivity = process.argv[2] || 100;
+// optput greeting
+console.log( '---------\n' );
+console.log( 'calculating closest fires from you, up to ' + options.threshold + ' km away.');
+console.log( 'lat: ' + options.lat + ' lon: ' + options.lon + '\n');
+// console.log( '---------\n' );
 
-// init xml2js, and prepare it to handle data from inciweb
+// ==================================================
+// Lat Long Functions
+// ==================================================
+/**
+ * Converts numeric degrees to radians
+ * http://www.movable-type.co.uk/scripts/latlong.html
+ */
+if (typeof(Number.prototype.toRadians) === "undefined") {
+  Number.prototype.toRadians = function() {
+    return this * Math.PI / 180;
+  };
+}
+
+/** 
+ * Calculates distance between two points on the earth's surface
+ * http://www.movable-type.co.uk/scripts/latlong.html
+ */
+function calcDistance( lat1, lon1, lat2, lon2 ){
+  var R = 6371000; // metres
+  var φ1 = lat1.toRadians();
+  var φ2 = lat2.toRadians();
+  var Δφ = (lat2-lat1).toRadians();
+  var Δλ = (lon2-lon1).toRadians();
+
+  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+    Math.cos(φ1) * Math.cos(φ2) *
+    Math.sin(Δλ/2) * Math.sin(Δλ/2);
+
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  var d = R * c;
+
+  return d / 1000; // output km
+}
+
+
+// ==================================================
+// XML Parser
+// ==================================================
 var parser = new xml2js.Parser();
 parser.addListener('end', function(result) {
   var json = result;
   var fires = json.rss.channel[0].item;
   
-  for (var i = fires.length - 1; i >= 0; i--) {
+  for ( var i = 0; i < fires.length; i++ ) {
     var lat = parseFloat( fires[i]["geo:lat"][0] ),
-      long = parseFloat( fires[i]["geo:long"][0] );
+      lon = parseFloat( fires[i]["geo:long"][0] );
 
-    if( lat && long ) {
-      // some entries don't have geo coordinates. please don't burn my house
-      if( Math.abs( myGeo.lat - lat ) < ( 0.1 * sensitivity ) &&  Math.abs( myGeo.long - long ) < ( 0.2 * sensitivity) ){
+    if( lat && lon ) {
+      var distance = calcDistance( lat, lon, options.lat, options.lon );
+
+      if( distance < options.threshold ) {
 
         console.log( '---------\n' + fires[i].title[0] + '\n' );
-        console.log( 'distance: ' + Math.abs( myGeo.lat - lat ) + ', ' + Math.abs( myGeo.long - long ) + '\n' );
+        console.log( 'distance: ' + distance.toPrecision(5)  + 'km ('+ ( distance * 0.621371 ).toPrecision(5) +' miles)\n' );
         console.log( 'link: ' + fires[i].link[0] + '\n');
         console.log( 'description: ' + fires[i].description[0] + '\n');
       }
@@ -41,7 +98,9 @@ parser.addListener('end', function(result) {
 
 });
 
-// callback for http
+// ==================================================
+// HTTP GET
+// ==================================================
 callback = function(response) {
   var str = '';
 
@@ -56,5 +115,8 @@ callback = function(response) {
   });
 };
 
-// make the request
 http.request(options, callback).end();
+
+// ==================================================
+// END
+// ==================================================
