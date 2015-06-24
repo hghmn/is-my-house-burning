a9c850ef2029b8e57f467c9dbcd7b17686b82728
hghmn <nathanwhughes@gmail.com>
2015-06-24 11:52:42 -0700

    Cleaned up code, adding get-opt to parse in command line args. Adding error catching for connection to feed.

--- main.js
+++ main.js
@@ -4,35 +4,54 @@
 
 // get feed from inciweb, and do something with it
 var http = require('http'),
-  xml2js = require('xml2js');
+  xml2js = require('xml2js'),
+  pjson = require('./package.json');
 
+// node-getopt  to scrape command line
+var opt = require('node-getopt').create([
+  ['t'  , 'threshold=ARG'   , 'threshold distance from center to search. Defaults to 100km.'],
+  ['l'  , 'latitude=ARG'    , 'latitude for center of search.' ],
+  ['n'  , 'longitude=ARG'   , 'longitude for center of search.' ],
+  ['h'  , 'help'            , 'display this help.'],
+  ['v'  , 'version'         , 'show version.']
+])              // create Getopt instance
+.bindHelp()     // bind option 'help' to default action
+.parseSystem(); // parse command line
+
+// if prompted for version, print and exit
+if( opt.options.version ) {
+  console.log( pjson.name + " - version: " + pjson.version );
+  process.exit();
+}
+
+// otherwise, continue
 // The url we want is: 'http://inciweb.nwcg.gov/feeds/rss/incidents/'
 var options = {
   host : 'inciweb.nwcg.gov',
   path : '/feeds/rss/incidents/',
-  threshold : 100, // 100 km
-  lat : 37.735969, // yosemite valley
-  lon : -119.601631
+  threshold : parseFloat( opt.options.threshold ) || 100, // 100 km
+  lat : parseFloat( opt.options.latitude )  || 37.735969, // yosemite valley
+  lon : parseFloat( opt.options.longitude ) || -119.601631
 };
 
-// scrape command line arguments
+/* // scrape command line arguments
 for ( var i = 0; i < process.argv.length; i+=2 ) {
-  if( /(-t|--threshold)/.test( process.argv[ i ] ) && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
+  if( /(-t|--threshold)/.test( process.argv[ i ] ) && process.argv[ i + 1 ] && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
     // get threshold
     options.threshold = parseFloat( process.argv[ i + 1 ] );
-  } else if( /(-lat|--latitute)/.test( process.argv[ i ] ) && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
+  } else if( /(-lat|--latitute)/.test( process.argv[ i ] ) && process.argv[ i + 1 ] && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
     // get latitute
     options.lat = parseFloat( process.argv[ i + 1 ] );
-  } else if( /(-lon|--longitude)/.test( process.argv[ i ] ) && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
+  } else if( /(-lon|--longitude)/.test( process.argv[ i ] ) && process.argv[ i + 1 ] && "NaN" !== parseFloat( process.argv[ i + 1 ] ) ) {
     // get longitude
     options.lon = parseFloat( process.argv[ i + 1 ] );
   }
-}
+} */
 
 // optput greeting
-console.log( '---------\n' );
-console.log( 'calculating closest fires from you, up to ' + options.threshold + ' km away.');
-console.log( 'lat: ' + options.lat + ' lon: ' + options.lon + '\n');
+console.log( '----------\n' );
+console.log( 'calculating closest fires to you, up to ' + options.threshold + ' km away.' );
+console.log( 'lat: ' + options.lat + ' lon: ' + options.lon );
 
 // ==================================================
 // Lat Long Functions
@@ -77,31 +96,61 @@ var parser = new xml2js.Parser();
 parser.addListener('end', function(result) {
   var json = result;
   var fires = json.rss.channel[0].item;
+  var pubDate = json.rss.channel[0].pubDate[0];
+
+  // print published date - with localized date formatting
+  // compare that to the time that we checked for update
+  console.log( 'published date: ' + ( new Date( pubDate ) ) );
+  console.log( 'last checked  : ' + ( new Date() ) + '\n' );
   
-  for ( var i = 0; i < fires.length; i++ ) {
+  for ( var i=0, j=0; i < fires.length; i++ ) {
     var lat = parseFloat( fires[i]["geo:lat"][0] ),
       lon = parseFloat( fires[i]["geo:long"][0] );
 
     if( lat && lon ) {
-      var distance = calcDistance( lat, lon, options.lat, options.lon );
+      var distance = calcDistance( lat, lon, options.lat, options.lon ),
+        size = fires[i].description ? fires[i].description[0].match( /([\d,]+ acres)/ ) : false,
+        direction = ( options.lat - lat > 0 ? 's' : 'n' ) + ( options.lon - lon > 0 ? 'w' : 'e' );
+
+        // 
+        j++;
 
       if( distance < options.threshold ) {
+        var output = '----------\n' + fires[i].title[0] + '\n' +
+          'distance  : ' + distance.toPrecision(5)  + 'km ('+ ( distance * 0.621371 ).toPrecision(5) +' miles)' +
+          'direction : ' + direction +
+          'fire size : ' + ( size ? size[1] : 'no info' ) +
+          'info link : ' + fires[i].link[0] + '\n' +
+          fires[i].description[0] + '\n';
 
-        console.log( '---------\n' + fires[i].title[0] + '\n' );
-        console.log( 'distance: ' + distance.toPrecision(5)  + 'km ('+ ( distance * 0.621371 ).toPrecision(5) +' miles)\n' );
-        console.log( 'link: ' + fires[i].link[0] + '\n');
-        console.log( 'description: ' + fires[i].description[0] + '\n');
+        console.log( output );
       }
     }
   }
 
+  // 
+  console.log( '----------\n' + j + ' fires checked out of ' + i + ' total\n----------' );
 });
 
 // ==================================================
 // HTTP GET
 // ==================================================
+
+function printConnectionError () {
+  console.log( '----------\nerror connecting to feed at ' + options.host + ' \n----------' );
+  process.exit();
+}
+
 callback = function(response) {
   var str = '';
+
+  response.on('error', function(exception){
+    printConnectionError();
+  });
+
+  if ( response.statusCode > '400' ){
+    printConnectionError();
+  }
 
   //another chunk of data has been recieved, so append it to `str`
   response.on('data', function (chunk) {
@@ -114,7 +163,9 @@ callback = function(response) {
   });
 };
 
-http.request(options, callback).end();
+http.request(options, callback)
+.on('error', printConnectionError )
+.end();
 
 // ==================================================
 // END
